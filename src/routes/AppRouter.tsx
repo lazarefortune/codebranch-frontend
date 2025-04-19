2d39782ad6a277e44e718e3bf019988e255353fe
Lazare Fortune <lazarefortune@gmail.com>
2025-04-19 22:09:18 +0200

    feat: add dashboard page and private routing (chore/design)
    - Implement dashboard page component.
    - Introduce private routes for authenticated user access.
    - Configure routing for dashboard in AppRouter.

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -4,15 +4,25 @@ import NotFound from "../pages/NotFound";
 import Layout from "../components/Layout.tsx";
 import Login from "../pages/Login.tsx";
 import Register from "../pages/Register.tsx";
+import PrivateRoute from "@/components/auth/PrivateRoute";
+import Dashboard from "@/pages/Dashboard";
 
 const AppRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route element={<Layout />}>
+                    {/* Public */}
                     <Route path="/" element={<Home />} />
                     <Route path="/connexion" element={<Login />} />
                     <Route path="/inscription" element={<Register />} />
+
+                    {/* Private */}
+                    <Route element={<PrivateRoute/>}>
+                        <Route path="/dashboard" element={<Dashboard/>}/>
+                    </Route>
+
+                    {/* Error */}
                     <Route path="*" element={<NotFound />} />
                 </Route>
             </Routes>
