7a6d4b2d13833c9523500e9532449c5b846edacd
Lazare Fortune <lazarefortune@gmail.com>
2025-06-13 01:01:46 +0200

    chore: update registration API endpoint in auth.ts

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -31,7 +31,7 @@ export const login = async (email:string, password: string) => {
 }
 
 export const register = async (username: string, email: string, password: string) => {
-    const response = await axios.post(`${API_URL}/users`, {
+    const response = await axios.post(`${API_URL}/auth/register`, {
         username,
         email,
         password,
